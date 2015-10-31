bf64aaeac2e4a6b0cc74cfdb915c89de3b726eb6
codeamac08 <codeamac08@gmail.com>
2015-10-30 18:57:30 -0600

    Ejercicio de almacenar informacion

---
+++ csv_como_almacenar_informacion.rb
@@ -0,0 +1,48 @@
+require "csv"
+require "faker"
+
+class Person
+	attr_accessor :first_name, :last_name, :email, :phone, :created_at
+	
+	def initialize(first_name, last_name, email, phone, created_at)
+		@first_name = first_name
+		@last_name = last_name
+		@email = email
+		@phone = phone
+		@created_at = created_at
+	end
+
+	def self.personas(number) 
+		persons = []
+		number.times do
+			persons << Person.new(Faker::Name.first_name, Faker::Name.last_name, Faker::Internet.email, Faker::PhoneNumber.phone_number, Time.now)
+		end
+		persons
+	end
+end
+
+
+
+class PersonWriter
+
+	def initialize(file, people)
+		@file = file
+		@people = people
+	end
+	
+	def create_csv 
+
+		CSV.open(@file, "wb") do |csv|
+			@people.each do |persona| 
+				csv << [persona.first_name, persona.last_name, persona.email, persona.phone, persona.created_at]
+			end
+		end
+	end
+end
+
+# person_writer = PersonWriter.new
+# people = Person.new()
+
+people = Person.personas(8)
+person_writer = PersonWriter.new("people.csv", people)
+person_writer.create_csv
